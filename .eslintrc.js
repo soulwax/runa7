1e68228845ec6a0c18299960e0245fa32ae773af
soulwax <soulwax-95@protonmail.com>
2024-07-05 03:04:34 +0200

    add .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    env: {
+        node: true,
+        es2020: true,
+    },
+};
